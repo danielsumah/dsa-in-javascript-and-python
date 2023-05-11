1bec2aa70e7715d63f15cab9cf4664a70fbacdb6
danielsumah <danielsumah@gmail.com>
2023-05-11 18:05:17 +0100

    updates on palindrome

--- stack/isPalindrome.js
+++ stack/isPalindrome.js
@@ -1,20 +1,18 @@
-let wordToCheck = "1racecar1";
-let reversedWord = "";
-
+var wordToCheck = "1racecar1";
+var reversedWord = "";
 // push all the letters of the word to check into an array
-let letters = [];
-for (let i = 0; i < wordToCheck.length; i++) {
-  letters.push(wordToCheck[i]);
+var letters = [];
+for (var i = 0; i < wordToCheck.length; i++) {
+    letters.push(wordToCheck[i]);
 }
-
 // pop of the words in the reverse order
 // remove all the elements of the stack starting from the end and use to create the reversedWord
-for (let i = 0; i < wordToCheck.length; i++) {
-  reversedWord += letters.pop();
+for (var i = 0; i < wordToCheck.length; i++) {
+    reversedWord += letters.pop();
 }
-
 if (wordToCheck === reversedWord) {
-  console.log(wordToCheck, "is a palindrome");
-} else {
-  console.log(wordToCheck, "is not a palindrome");
+    console.log(wordToCheck, "is a palindrome");
+}
+else {
+    console.log(wordToCheck, "is not a palindrome");
 }
---
+++ stack/isPalindrome.ts
@@ -0,0 +1,20 @@
+let wordToCheck: string = "1racecar1";
+let reversedWord: string = "";
+
+// push all the letters of the word to check into an array
+let letters: Array<string> = [];
+for (let i = 0; i < wordToCheck.length; i++) {
+  letters.push(wordToCheck[i]);
+}
+
+// pop of the words in the reverse order
+// remove all the elements of the stack starting from the end and use to create the reversedWord
+for (let i = 0; i < wordToCheck.length; i++) {
+  reversedWord += letters.pop();
+}
+
+if (wordToCheck === reversedWord) {
+  console.log(wordToCheck, "is a palindrome");
+} else {
+  console.log(wordToCheck, "is not a palindrome");
+}
