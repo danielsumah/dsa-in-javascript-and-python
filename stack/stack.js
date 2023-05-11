d1885ef4a579d5edddd90319f0b1e2dae5b62158
danielsumah <danielsumah@gmail.com>
2023-05-11 17:52:09 +0100

    stacks/palindrome

---
+++ stack/stack.js
@@ -0,0 +1,7 @@
+// stack methods
+// 1. push => places data to the end/top of a atack
+// 2. pop => removes the last element in a stack
+// 3. peek => for displaying the top element of a stack
+// 4. length => for determining the length of a stack
+
+// /check if a word is a palindrome
