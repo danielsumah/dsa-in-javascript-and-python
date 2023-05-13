8c208df4144a2ac068077ab192b3bc93a39253f9
danielsumah <danielsumah@gmail.com>
2023-05-13 18:01:13 +0100

    Update mySet.js

--- set/mySet.js
+++ set/mySet.js
@@ -2,13 +2,6 @@ var MySet = /** @class */ (function () {
     function MySet() {
         this.collection = [];
     }
-    MySet.prototype.subset = function (Set2) {
-        var _this = this;
-        return Set2.every(function (value) { return _this.collection.includes(value); });
-    };
-    MySet.prototype.difference = function (Set2) {
-        throw new Error("Method not implemented.");
-    };
     MySet.prototype.values = function () {
         return this.collection;
     };
@@ -36,21 +29,42 @@ var MySet = /** @class */ (function () {
             var element = set1[i];
             mergedSet.add(element);
         }
-        for (var i = 0; i < Set2.length; i++) {
-            var element = Set2[i];
+        for (var i = 0; i < Set2.size(); i++) {
+            var element = Set2.values()[i];
             mergedSet.add(element);
         }
-        return mergedSet.values();
+        return mergedSet;
     };
     MySet.prototype.intersection = function (Set2) {
         var intersectionSet = new MySet();
-        for (var i = 0; i < Set2.length; i++) {
-            var element = Set2[i];
+        for (var i = 0; i < Set2.size(); i++) {
+            var element = Set2.values()[i];
             if (this.has(element)) {
                 intersectionSet.add(element);
             }
         }
-        return intersectionSet.values();
+        return intersectionSet;
+    };
+    MySet.prototype.subset = function (Set2) {
+        var _this = this;
+        return Set2.values().every(function (value) { return _this.collection.includes(value); });
+    };
+    MySet.prototype.difference = function (Set2) {
+        var diffSet = new MySet();
+        var Set1 = this.values();
+        for (var i = 0; i < this.size(); i++) {
+            var element = this.values()[i];
+            if (!Set2.has(element)) {
+                diffSet.add(element);
+            }
+        }
+        for (var i = 0; i < Set2.size(); i++) {
+            var element = Set2.values()[i];
+            if (!this.has(element)) {
+                diffSet.add(element);
+            }
+        }
+        return diffSet;
     };
     return MySet;
 }());
@@ -63,25 +77,21 @@ aNewSet.add(3);
 console.log(aNewSet.values());
 console.log("size is ", aNewSet.size());
 aNewSet.add("ToRemove");
-console.log("value to remove is last", aNewSet.values());
+console.log("\nvalue to remove is last", aNewSet.values());
 aNewSet.remove("ToRemove");
 console.log(aNewSet.values());
-console.log("does 3 exist? => ", aNewSet.has(3));
-console.log("does 3 ToRemove? => ", aNewSet.has("ToRemove"));
+console.log("\ndoes 3 exist in the first set? => ", aNewSet.has(3));
+console.log("\ndoes ToRemove exist? => ", aNewSet.has("ToRemove"));
 console.log("\nset 1 => ", aNewSet.values());
-var setToMerge = [3, "Daniel", 5, 6, "like"];
-console.log("set 2 => ", setToMerge);
+var setToMerge = new MySet();
+setToMerge.add(3);
+setToMerge.add("Daniel");
+setToMerge.add(5);
+setToMerge.add(6);
+setToMerge.add("like");
+console.log("\nset 2 => ", setToMerge.values());
 var mergedSet = aNewSet.merge(setToMerge);
-console.log("merged set => ", mergedSet);
-console.log("\nIntercetion of the sets => ", aNewSet.intersection(setToMerge));
-// subset test
-var mother = new MySet();
-mother.add(1);
-mother.add(2);
-mother.add(3);
-mother.add(4);
-console.log("mother is => ", mother);
-var child = new MySet();
-child.add(2);
-child.add(3);
-console.log("is child subset of mother", mother.subset(child.values()));
+console.log("\nmerged set => ", mergedSet.values());
+console.log("\nIntercetion of the sets => ", aNewSet.intersection(setToMerge).values());
+console.log("\nDifference => ", aNewSet.difference(setToMerge).values());
+console.log("\nIs arg a subSet? => ", aNewSet.subset(setToMerge));
